ab2b4a5afc603354a958c34d126f8102baa04481
zaszen.dev <zaszen.dev@gmail.com>
2024-10-14 22:11:07 +0200

    dotnet format

--- CleanArchitecture.Api/Program.cs
+++ CleanArchitecture.Api/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Host.UseSerilog((context, config) => 
+builder.Host.UseSerilog((context, config) =>
     config.ReadFrom.Configuration(context.Configuration)
         .Enrich.With(new ThreadPriorityEnricher())
         .Enrich.FromLogContext()
